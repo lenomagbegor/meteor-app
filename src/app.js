937536447b15e39671c60b49cd71d8d37a8a99f3
lenomagbegor <lenomaggy01@gmail.com>
2024-05-13 09:46:19 +0100

    Added the search functionality using the Weather API
    Using JS querySelector, Weather API and axios, I was able to change the elements in HTML to the user's city and current temperature of that city

--- src/app.js
+++ src/app.js
@@ -1,9 +1,26 @@
+function displayWeather(response) {
+  let temperature1 = Math.round(response.data.temperature.current);
+  let temperatureElement1 = document.querySelector("#temp1");
+  temperatureElement1.innerHTML = `${temperature1}℃`;
+
+  let userCity = document.querySelector("#city");
+  userCity.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "08452154bta00ao7c030df2ccc330f5e";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
 function searchForm(event) {
   event.preventDefault();
   let userInput = document.querySelector("#search-input");
-  let userCity = document.querySelector("#city");
-  userCity.innerHTML = userInput.value;
+
+  searchCity(userInput.value);
 }
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", searchForm);
+
+searchCity("Lagos");
